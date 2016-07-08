a4c3573fa891a720290c86704c51bba91948c4b9
Paul Chicarello <pchicarello@patientslikeme.com>
2016-07-08 16:17:26 -0400

    Provisioning in Digital Ocean now works\!

--- wedding_website/digital_ocean/prod/gavi.tf
+++ wedding_website/digital_ocean/prod/gavi.tf
@@ -2,5 +2,22 @@ resource "digitalocean_droplet" "gavi" {
     image = "ubuntu-14-04-x64"
     name = "gavi"
     region = "nyc2"
-    size = "512mb"
+    size = "1gb"
+    ssh_keys = ["17:27:51:3d:95:6f:7f:ce:8c:96:be:81:02:7f:3a:0f"]
+
+    connection {
+      user = "root"
+      private_key = "${file("../../../private/ssh_key.pem")}"
+    }
+
+    provisioner "chef" {
+      environment = "wedding-stage"
+      run_list = ["pjc-base::default"]
+      node_name = "gavi"
+
+      server_url = "${var.chef_url}"
+      validation_client_name = "${var.chef_client_name}"
+      validation_key = "${file("../../../private/chef_key.pem")}"
+      version = "12.9.41"
+    }
 }
--- wedding_website/digital_ocean/prod/variables.tf
+++ wedding_website/digital_ocean/prod/variables.tf
@@ -1,1 +1,3 @@
 variable "do_token" {}
+variable "chef_url" { default = "https://api.chef.io/organizations/chicarello" }
+variable "chef_client_name" {}
